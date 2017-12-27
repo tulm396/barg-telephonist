e20fe60e910c5370eda5807834a9bfe1e4fb631e
Le Minh Tu <tulm396@gmail.com>
2017-12-27 18:25:27 +0700

    Add project

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+
+var express = require('express');
+var path	= require('path');
+var app		= express();
+
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile('./dist/pages/index.html', {
+        root: __dirname
+    });
+});
+
+app.get('*', (req, res) => {
+	res.redirect('/');
+});
+
+// Create http server
+app.listen(8080);
